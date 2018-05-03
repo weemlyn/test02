812272b9d57e3957a2bb9f72803c618639422989
weemlyn <1067718901@qq.com>
2018-05-03 11:48:09 +0800

    change from teaching

--- test02/src/test02/GradeLevel.java
+++ test02/src/test02/GradeLevel.java
@@ -9,19 +9,27 @@ public class GradeLevel {
 		System.out.println("请输入学生成绩：");
 		Scanner grade = new Scanner(System.in);
 		int a = grade.nextInt();
-		if(a>=90) {
+		
+		//if语句只输出一个分支
+		//
+		
+		if(a>100||a<0) {
+			System.out.println("请输入正确的成绩");
+			return;
+		}
+		if (a>=90) {
 			System.out.println("一个优秀的分数,你好棒啊");
 		}
-		if(a<90&a>=80) {
+		else if(a>=80) {
 			System.out.println("一个评为良好的分数,加油啊");
 		}
-		if(a<80&a>=70) {
+		else if(a>=70) {
 			System.out.println("一个评为良的分数，再加把劲");
 		}
-		if(a<70&a>=60) {
+		else if(a>=60) {
 			System.out.println("一个刚刚及格的分数，还需努力");
 		}
-		if(a<60) {
+		else {
 			System.out.println("这真是一个杯具");
 		}
 		grade.close();
